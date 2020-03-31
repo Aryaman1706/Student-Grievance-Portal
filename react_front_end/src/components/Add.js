fc93b32d7ac197172290a92ac18de89848b528d6
aniketbiswas21 <aniket.biswas75@gmail.com>
2020-03-31 16:31:08 +0530

    Fixed some bugs

--- react_front_end/src/components/Add.js
+++ react_front_end/src/components/Add.js
@@ -18,13 +18,13 @@ class Add extends Component {
     render(){
      return (
         <Fragment>
-            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous"></link>
+            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossOrigin="anonymous"></link>
                 <div className="container">
             <h1 className="text-center">ADD GRIEVANCE</h1>
             <br/>
             <form onSubmit={this.submit}>
                 <div className="form-group">
-                <label for="category">Category</label>
+                <label htmlFor="category">Category</label>
                 <select className="form-control" id="category" onChange={this.changeState}>
                     <option selected>Choose..</option>
                     <option>Academics</option>
@@ -35,17 +35,17 @@ class Add extends Component {
                 </div>
 
                 <div className="form-group">
-                <label for="subject">Subject:-</label>
+                <label htmlFor="subject">Subject:-</label>
                 <textarea className="form-control" id="subject" rows="3" onChange={this.changeState}></textarea>
                 </div>
 
                 <div className="form-group">
-                <label for="statement">Brief Description:-</label>
+                <label htmlFor="statement">Brief Description:-</label>
                 <textarea className="form-control" id="statement" rows="3" onChange={this.changeState}></textarea>
                 </div>
 
                 <div className="form-group">
-                <label for="files">Attach Files (optional):-</label>
+                <label htmlFor="files">Attach Files (optional):-</label>
                 <input type="file" className="form-control-file" id="files"/>
                 </div>
 
